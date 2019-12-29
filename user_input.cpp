771053db07e59428d62ff08fd5d805dc1fc65e23
YOURMATADOR <jesus.amancilla@alumnos.udg.mx>
2019-12-29 01:45:18 -0600

    configure c++ in vscode

---
+++ user_input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "insert your name plss" << endl;
+    string user_input;
+    cin >> user_input;
+    cout << user_input << endl;
+    return 0;
+}
